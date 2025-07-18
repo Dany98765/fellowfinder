520efae0dcf0e5e8371c6feb341067db2313aa47
Andy Nader <andynader@Andys-MacBook-Air.local>
2025-07-18 22:29:47 +0300

    J

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,17 +1,16 @@
 import Signup from "@/components/pages/auth/signup/page";
 import AuthSidebar from "@/components/sidebar/page";
 
-export const dynamic = 'force-dynamic';
 
 export default async function SignupPage({ searchParams }){
     // const searchParam = await searchParams
-    // const lang = await searchParam.lang; // replace with actual dynamic logic
-    const { lang } = await searchParams
+    //const lang = await searchParams.lang || 'en' // replace with actual dynamic logic
+    //const { lang } = await searchParams
+    const lang = await searchParams.lang; // replace with actual dynamic logic
     const direction = await lang === 'ar' ? 'rtl' : 'ltr';
     return(
         <div lang={lang} dir={direction}>
             <Signup lang={lang}/>
-            <AuthSidebar />
         </div>
     )
 }
--- components/sidebar/page.js
+++ components/sidebar/page.js
@@ -4,7 +4,7 @@ export default async function AuthSidebar({ lang }){
     let locale = lang || 'en'
     const messages = (await import(`@/public/locales/${locale}/auth/sidebar.json`)).default;
     let marginDirection = lang == 'en' ? 'marginLeft' : 'marginRight'
-    console.log(locale + "???????????????")
+    console.log(JSON.stringify(messages.title) + "???????????????")
     return(
         <div className="sidebarContainer" style={{ marginRight: "20px" }}>
             <h3 className="title" style={{ [marginDirection]: "20px" }}>{messages.title}</h3>
